f8edee865c3b585d7351494513e035d8636083ba
Facundo Menzella <fmenzella@monits.com>
2015-07-20 14:57:39 -0300

    LoginViewController created

---
+++ UnDiaParaDar/UnDiaParaDar/LoginModule/ViewController/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 7/20/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ UnDiaParaDar/UnDiaParaDar/LoginModule/ViewController/LoginViewController.m
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.m
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 7/20/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@implementation LoginViewController
+
+@end
