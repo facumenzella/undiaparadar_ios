68b712bc838db4b764666bf5bf88c715f8cbccfe
Facundo Menzella <fmenzella@monits.com>
2015-07-20 14:59:40 -0300

    login module assembly created

---
+++ UnDiaParaDar/UnDiaParaDar/LoginModule/Assembly/LoginModuleAssembly.h
@@ -0,0 +1,13 @@
+//
+//  LoginModuleAssembly.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 7/20/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import "TyphoonAssembly.h"
+
+@interface LoginModuleAssembly : TyphoonAssembly
+
+@end
---
+++ UnDiaParaDar/UnDiaParaDar/LoginModule/Assembly/LoginModuleAssembly.m
@@ -0,0 +1,13 @@
+//
+//  LoginModuleAssembly.m
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 7/20/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import "LoginModuleAssembly.h"
+
+@implementation LoginModuleAssembly
+
+@end
