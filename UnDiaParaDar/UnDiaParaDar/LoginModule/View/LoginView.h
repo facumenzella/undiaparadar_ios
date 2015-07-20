4b91bb5aaa0726ebd1c3e9b707f1832327cbb08c
Facundo Menzella <fmenzella@monits.com>
2015-07-20 16:20:49 -0300

    LoginView added to project

---
+++ UnDiaParaDar/UnDiaParaDar/LoginModule/View/LoginView.h
@@ -0,0 +1,13 @@
+//
+//  LoginView.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 7/20/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginView : UIView
+
+@end
---
+++ UnDiaParaDar/UnDiaParaDar/LoginModule/View/LoginView.m
@@ -0,0 +1,13 @@
+//
+//  LoginView.m
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 7/20/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import "LoginView.h"
+
+@implementation LoginView
+
+@end
