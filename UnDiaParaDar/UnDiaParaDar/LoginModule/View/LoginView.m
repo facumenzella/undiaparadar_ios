59a6520cc24036a7641df496f6128408797f0be2
Facundo Menzella <fmenzella@monits.com>
2015-07-20 16:24:23 -0300

    autolayout library added to view

--- UnDiaParaDar/UnDiaParaDar/LoginModule/View/LoginView.m
+++ UnDiaParaDar/UnDiaParaDar/LoginModule/View/LoginView.m
@@ -8,6 +8,26 @@
 
 #import "LoginView.h"
 
+#import <UIView+AutoLayout.h>
+
 @implementation LoginView
+
+#pragma mark - LoginView
+
+- (instancetype) init
+{
+    self = [super init];
+    if (self) {
+        [self buildSubviews];
+    }
+    return self;
+}
+
+#pragma mark - BuildSubviews
+
+- (void)buildSubviews
+{
+    
+}
 
 @end
