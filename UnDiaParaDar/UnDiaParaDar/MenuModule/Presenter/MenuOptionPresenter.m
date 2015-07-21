a2282f21a911f29ebedcbc73ddc62ab18d34455f
Facundo Menzella <fmenzella@monits.com>
2015-07-21 19:52:56 -0300

    MenuOptionPresenter created

---
+++ UnDiaParaDar/UnDiaParaDar/MenuModule/Presenter/MenuOptionPresenter.m
@@ -0,0 +1,13 @@
+//
+//  MenuOptionPresenter.m
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 7/21/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import "MenuOptionPresenter.h"
+
+@implementation MenuOptionPresenter
+
+@end
