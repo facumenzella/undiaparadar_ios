e16fdd2298bac06da29db01ffc6013cd21d05831
Facundo Menzella <fmenzella@monits.com>
2015-07-21 19:52:05 -0300

    ProfileOptionPresenter created

---
+++ UnDiaParaDar/UnDiaParaDar/MenuModule/Presenter/ProfileOptionPresenter.h
@@ -0,0 +1,13 @@
+//
+//  ProfileOptionPresenter.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 7/21/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import "RETableViewItem.h"
+
+@interface ProfileOptionPresenter : RETableViewItem
+
+@end
