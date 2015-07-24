7dcc2af97f867bf29542dce19383d8e1f3924427
Facundo Menzella <fmenzella@monits.com>
2015-07-24 16:21:45 -0300

    Tailor arrives to undiaparadar

---
+++ UnDiaParaDar/UnDiaParaDar/RoutingModule/SWRevealTailor.h
@@ -0,0 +1,14 @@
+//
+//  SWRevealTailor.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 7/24/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Tailor.h"
+
+@interface SWRevealTailor : NSObject <Tailor>
+
+@end
---
+++ UnDiaParaDar/UnDiaParaDar/RoutingModule/SWRevealTailor.m
@@ -0,0 +1,18 @@
+//
+//  SWRevealTailor.m
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 7/24/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import "SWRevealTailor.h"
+
+@implementation SWRevealTailor
+
+- (void)prepareViewController:(id)viewController
+{
+    
+}
+
+@end
