2508345457056040e4252e1cb0900e8adc73cccd
Facundo <fmenzella@monits.com>
2015-08-14 18:26:04 -0300

    topic basic model added

---
+++ UnDiaParaDar/UnDiaParaDar/Model/Topic.h
@@ -0,0 +1,16 @@
+//
+//  Topic.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 8/14/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Topic : NSObject
+
+@property (nonatomic, strong) NSString *img;
+@property (nonatomic, strong) NSString *name;
+
+@end
---
+++ UnDiaParaDar/UnDiaParaDar/Model/Topic.m
@@ -0,0 +1,13 @@
+//
+//  Topic.m
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 8/14/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import "Topic.h"
+
+@implementation Topic
+
+@end
