e3b5f7d0fb7c14ce524d3f3f2c1f66510f971320
Facundo <fmenzella@monits.com>
2015-09-03 15:24:42 -0300

    button factory added, not integrated yet

---
+++ UnDiaParaDar/UnDiaParaDar/Factories/ButtonFactory.h
@@ -0,0 +1,15 @@
+//
+//  ButtonFactory.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 9/3/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ButtonFactory : NSObject
+
+
+
+@end
---
+++ UnDiaParaDar/UnDiaParaDar/Factories/ButtonFactory.m
@@ -0,0 +1,13 @@
+//
+//  ButtonFactory.m
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 9/3/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import "ButtonFactory.h"
+
+@implementation ButtonFactory
+
+@end
