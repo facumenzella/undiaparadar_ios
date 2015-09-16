d4c7c6cffcd4630d7e52e3fc56df190018fa3123
Facundo <fmenzella@monits.com>
2015-09-16 16:40:03 -0300

    positiveActionsWithMapView created but not integrated

---
+++ UnDiaParaDar/UnDiaParaDar/TopicsModule/View/PositiveActionsWithMapView.h
@@ -0,0 +1,13 @@
+//
+//  PositiveActionsWithMapView.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 9/16/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PositiveActionsWithMapView : UIView
+
+@end
---
+++ UnDiaParaDar/UnDiaParaDar/TopicsModule/View/PositiveActionsWithMapView.m
@@ -0,0 +1,22 @@
+//
+//  PositiveActionsWithMapView.m
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 9/16/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import "PositiveActionsWithMapView.h"
+
+@implementation PositiveActionsWithMapView
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
