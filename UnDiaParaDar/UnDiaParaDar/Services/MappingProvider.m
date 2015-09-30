071ce39bc709805e966e62417c3cf0a9b89e78ec
Facundo <fmenzella@monits.com>
2015-09-29 22:14:40 -0300

    positiveActionMapping added to mappingProvider

--- UnDiaParaDar/UnDiaParaDar/Services/MappingProvider.m
+++ UnDiaParaDar/UnDiaParaDar/Services/MappingProvider.m
@@ -7,12 +7,31 @@
 //
 
 #import "MappingProvider.h"
+#import "PositiveAction.h"
 
 @implementation MappingProvider
 
 - (RKMapping*)positiveActionMapping
 {
-    return nil;
+    RKObjectMapping *mapping = [RKObjectMapping mappingForClass:[PositiveAction class]];
+    
+    [mapping addAttributeMappingsFromDictionary:@{
+                                                  @"id": @"positiveActionID",
+                                                  @"latlng_0_coordinate": @"latitude",
+                                                  @"latlng_1_coordinate": @"longitude",
+                                                  @"video_url": @"videoURL",
+                                                  @"external_url": @"externalURL",
+                                                  @"title": @"title",
+                                                  @"subtitle": @"subtitle",
+                                                  @"desc": @"positiveActionDescription",
+                                                  @"ong_name": @"ongName",
+                                                  @"ong_id": @"ongID",
+                                                  @"topics": @"topicID",
+                                                  @"country": @"country",
+                                                  @"city": @"city"
+                                                  }];
+    return mapping;
+    
 }
 
 @end
