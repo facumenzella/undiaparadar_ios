8da05678a88d5f5fa3f393ada7a61df8bb70e313
Facundo <fmenzella@monits.com>
2015-09-29 22:00:00 -0300

    MappingProvider created but no methods added

---
+++ UnDiaParaDar/UnDiaParaDar/Services/MappingProvider.h
@@ -0,0 +1,16 @@
+//
+//  MappingProvider.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 9/29/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <RestKit/RestKit.h>
+
+@interface MappingProvider : NSObject
+
+- (RKMapping*)positiveActionMapping;
+
+@end
---
+++ UnDiaParaDar/UnDiaParaDar/Services/MappingProvider.m
@@ -0,0 +1,18 @@
+//
+//  MappingProvider.m
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 9/29/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import "MappingProvider.h"
+
+@implementation MappingProvider
+
+- (RKMapping*)positiveActionMapping
+{
+    return nil;
+}
+
+@end
