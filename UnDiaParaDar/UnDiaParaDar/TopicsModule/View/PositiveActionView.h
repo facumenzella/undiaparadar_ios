0936198df1ab0327e55fd10aeba0ff62c017c548
Facundo <fmenzella@monits.com>
2015-10-03 19:46:13 -0300

    PositiveActionView created

---
+++ UnDiaParaDar/UnDiaParaDar/TopicsModule/View/PositiveActionView.h
@@ -0,0 +1,13 @@
+//
+//  PositiveActionView.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 10/3/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PositiveActionView : UIView
+
+@end
