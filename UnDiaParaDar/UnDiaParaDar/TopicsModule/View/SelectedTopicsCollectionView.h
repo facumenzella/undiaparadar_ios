052b7ad99e2fa6e139bd19407b6c45185d9d279e
Facundo <fmenzella@monits.com>
2015-10-07 23:22:45 -0300

    SelectedTopicCollectionViewCell added

---
+++ UnDiaParaDar/UnDiaParaDar/TopicsModule/View/SelectedTopicsCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  SelectedTopicsCollectionView.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 10/7/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectedTopicsCollectionView : UICollectionView
+
+@end
