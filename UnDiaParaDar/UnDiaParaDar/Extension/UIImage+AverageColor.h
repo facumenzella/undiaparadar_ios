a0eed039ff0dd730120d170932756658a6bee75e
Facundo <fmenzella@monits.com>
2015-10-07 23:22:01 -0300

    average color of uiimage category added

---
+++ UnDiaParaDar/UnDiaParaDar/Extension/UIImage+AverageColor.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+AverageColor.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 10/7/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (AverageColor)
+
+- (UIColor *)averageColor;
+
+@end
