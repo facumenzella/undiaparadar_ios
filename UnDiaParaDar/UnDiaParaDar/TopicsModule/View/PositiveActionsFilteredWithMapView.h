59afde6cd7c575089240669f6eedb38749462d08
Facundo <fmenzella@monits.com>
2015-10-14 12:47:35 -0300

    added all strings to localizable.strings as default

---
+++ UnDiaParaDar/UnDiaParaDar/TopicsModule/View/PositiveActionsFilteredWithMapView.h
@@ -0,0 +1,13 @@
+//
+//  PositiveActionsFilteredWithMapView.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 10/14/15.
+//  Copyright (c) 2015 ITBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PositiveActionsFilteredWithMapView : UIView
+
+@end
