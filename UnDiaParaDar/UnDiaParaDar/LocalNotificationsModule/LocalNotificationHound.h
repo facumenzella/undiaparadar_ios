4a47c5dd4f59264ca8845f4b5706380005839c8c
Facundo <fmenzella@monits.com>
2016-02-07 19:43:15 -0300

    localnotifications hound created

---
+++ UnDiaParaDar/UnDiaParaDar/LocalNotificationsModule/LocalNotificationHound.h
@@ -0,0 +1,15 @@
+//
+//  LocalNotificationHound.h
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 2/7/16.
+//  Copyright © 2016 ITBA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocalNotificationHound : NSObject
+
+- (void)setup;
+
+@end
---
+++ UnDiaParaDar/UnDiaParaDar/LocalNotificationsModule/LocalNotificationHound.m
@@ -0,0 +1,41 @@
+//
+//  LocalNotificationHound.m
+//  UnDiaParaDar
+//
+//  Created by Facundo Menzella on 2/7/16.
+//  Copyright © 2016 ITBA. All rights reserved.
+//
+
+#import "LocalNotificationHound.h"
+#import <UIKit/UILocalNotification.h>
+#import <UIKit/UIApplication.h>
+
+@interface LocalNotificationHound ()
+
+@property (nonatomic, strong) NSDate *date;
+@property (nonatomic, strong) NSString *msg;
+
+@end
+
+@implementation LocalNotificationHound
+
+- (instancetype)initWithDate:(NSDate*)date withMessage:(NSString*)msg
+{
+    self = [super init];
+    if (self) {
+        self.date = date;
+        self.msg = msg;
+    }
+    return self;
+}
+
+- (void)setup
+{
+    UILocalNotification* localNotification = [[UILocalNotification alloc] init];
+    localNotification.fireDate = self.date;
+    localNotification.alertBody = self.msg;
+    localNotification.timeZone = [NSTimeZone defaultTimeZone];
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+}
+
+@end
